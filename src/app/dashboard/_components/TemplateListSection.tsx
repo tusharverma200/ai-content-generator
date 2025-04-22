ad5ce84b408b3012cd04967c70edc809bc93f50b
Tushar Verma <vermatushar200@gmail.com>
2025-04-22 14:30:12 +0530

    TemplateListSection update

--- src/app/dashboard/_components/TemplateListSection.tsx
+++ src/app/dashboard/_components/TemplateListSection.tsx
@@ -4,43 +4,47 @@ import Templates from '@/app/(data)/Templates'
 import React, { useEffect, useState } from 'react'
 import TemplateCard from './TemplateCard'
 
-export interface TEMPLATE{
-  name:string,
-  desc:string,
-  category:string,
-  icon:string,
-  aiPrompt:string,
-  slug:string,
-  form?:FORM[]
+export interface TEMPLATE {
+  name: string,
+  desc: string,
+  category: string,
+  icon: string,
+  aiPrompt: string,
+  slug: string,
+  form?: FORM[]
 }
 
-export interface FORM{
-  label:string,
-  field:string,
-  name:string,
-  required?:boolean
+export interface FORM {
+  label: string,
+  field: string,
+  name: string,
+  required?: boolean
 }
 
-function TemplateListSection({userSearchInput}:any) {
+interface TemplateListSectionProps {
+  userSearchInput: string | undefined; // or whatever type userSearchInput actually is
+}
+
+function TemplateListSection({ userSearchInput }: TemplateListSectionProps) {
 
   const [filterData, setfilterData] = useState(Templates)
 
   useEffect(() => {
-    if(userSearchInput){
+    if (userSearchInput) {
 
-  const filter = Templates.filter((item)=>item.name.toLowerCase().includes(userSearchInput.toLowerCase()))
-setfilterData(filter)
+      const filter = Templates.filter((item) => item.name.toLowerCase().includes(userSearchInput.toLowerCase()))
+      setfilterData(filter)
     }
-    else{
+    else {
       setfilterData(Templates)
     }
   }, [userSearchInput])
-  
+
 
   return (
     <div className='grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5 p-10 bg-gray-100 ' >
-      {filterData.map((item:TEMPLATE, index:number)=>(
-   <TemplateCard key={index} {...item}/>
+      {filterData.map((item: TEMPLATE, index: number) => (
+        <TemplateCard key={index} {...item} />
       ))}
     </div>
   )
